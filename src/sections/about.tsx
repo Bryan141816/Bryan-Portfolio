0cd88191548606c4eda1b2d471d047baf5546a3e
Bryan Antier <bryanantier7@gmail.com>
2026-08-26 20:47:40 +0800

    feat: make page responsive

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,81 +1,189 @@
 export default function About() {
   return (
-    <div className="grid w-full min-h-screen grid-cols-3 grid-rows-[auto_auto] content-start p-20 gap-5">
-      <div className="col-span-3 flex flex-col gap-2 mb-20">
-        <h3 className="font-mono text-2xl font-bold">//ABOUT ME</h3>
+    <div
+      className="
+        grid
+        w-full
+        min-h-screen
+        grid-cols-2
+        md:grid-cols-3
+        content-start
+        gap-2
+        md:gap-5
+        p-5
+        sm:p-7
+        md:p-10
+        lg:p-15
+        xl:p-20
+        [&_h3]:font-mono
+        [&_h3]:text-base
+        [&_h3]:font-bold
+        md:[&_h3]:text-xl
+        lg:[&_h3]:text-2xl
 
-        <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-          Hello I'm Bryan Antier - A aspiring software developer with experience building full-stack web application, REST APIs, desktop applications
-          and mobile applications. Proficient in React, Next.js ASP.NET Core, Laravel, FastAPI and relational databases, with
-          hands-on experience in cloud deployment, CI/CD, and infrastructure using Microsoft Azure, GitHub Actions, and
-          Terraform. Familiar with Agile software development practices, including iterative development, collaboration,
-          sprint planning, and continuous improvement.
+        [&_p]:font-mono
+        [&_p]:text-sm
+     
+        [&_p]:uppercase
+        [&_p]:tracking-tighter
+        [&_p]:text-gray-400
+        md:[&_p]:text-sm
+        lg:[&_p]:text-xl
+
+        [&_b]:text-black
+      "
+    >
+      {/* ABOUT ME */}
+      <div className="col-span-2 mb-0 flex flex-col gap-2 md:col-span-3 md:mb-20">
+        <h3>//ABOUT ME</h3>
+
+        <p className="text-justify">
+          Hello I'm Bryan Antier - A aspiring software developer with
+          experience building full-stack web application, REST APIs, desktop
+          applications and mobile applications. Proficient in React, Next.js
+          ASP.NET Core, Laravel, FastAPI and relational databases, with
+          hands-on experience in cloud deployment, CI/CD, and infrastructure
+          using Microsoft Azure, GitHub Actions, and Terraform. Familiar with
+          Agile software development practices, including iterative
+          development, collaboration, sprint planning, and continuous
+          improvement.
         </p>
       </div>
-      <h3 className="font-mono text-2xl font-bold mb-20">
+
+      {/* EXPERIENCE */}
+      <h3 className="col-span-2 mb-0 md:col-span-1 md:mb-20">
         //Experience
       </h3>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">01.2026 - 7.2026</b><br />
+
+      <p>
+        <b>01.2026 - 7.2026</b>
+        <br />
         RAFI MFI
       </p>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">SOFTWARE ENGINEER</b><br />
+
+      <p>
+        <b>SOFTWARE ENGINEER</b>
+        <br />
         INTERN
       </p>
-      <h3 className="font-mono text-2xl font-bold ">
+
+      {/* TECH STACK */}
+      <h3 className="col-span-2 md:col-span-1">
         //TECH STACK
       </h3>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">{"<Language/>"}</b><br />
-        JavaScript<br />
-        TypeScript<br />
-        C#<br />
-        Python<br />
-        Dart<br />
-        SQL<br />
-        PHP<br />
+
+      <p>
+        <b>{"<Language/>"}</b>
+        <br />
+        JavaScript
+        <br />
+        TypeScript
+        <br />
+        C#
+        <br />
+        Python
+        <br />
+        Dart
+        <br />
+        SQL
+        <br />
+        PHP
+        <br />
       </p>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">{"<Frontend/>"}</b><br />
-        React<br />
-        Vue.js<br />
-        Next.js<br />
-        HTML<br />
-        CSS<br />
-        Tailwind CSS<br />
+
+      <p>
+        <b>{"<Frontend/>"}</b>
+        <br />
+        React
+        <br />
+        Vue.js
+        <br />
+        Next.js
+        <br />
+        HTML
+        <br />
+        CSS
+        <br />
+        Tailwind CSS
+        <br />
         ShadCN UI
       </p>
-      <div></div>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">{"<Backend/>"}</b><br />
-        ASP.Net Core<br />
-        FastAPI<br />
-        Django<br />
-        Laravel <br />
+
+      {/* Empty grid cell on desktop */}
+      <div className="hidden md:block"></div>
+
+      <p>
+        <b>{"<Backend/>"}</b>
+        <br />
+        ASP.Net Core
+        <br />
+        FastAPI
+        <br />
+        Django
+        <br />
+        Laravel
+        <br />
         Express.js
       </p>
 
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">{"<Database/>"}</b><br />PostgreSQL<br /> MySQL<br /> SQL Server
-      </p>
-      <div></div>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        <b className="text-black">{"<Orm/>"}</b><br />Entity Framework Core<br /> Prisma<br /> Eloquent
-      </p>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400 mb-20">
-        <b className="text-black">{"<Mobile/>"}</b><br />Flutter<br />Jetpack Compose
+      <p>
+        <b>{"<Database/>"}</b>
+        <br />
+        PostgreSQL
+        <br />
+        MySQL
+        <br />
+        SQL Server
       </p>
 
-      <h3 className="font-mono text-2xl font-bold mb-20">
+      {/* Empty grid cell on desktop */}
+      <div className="hidden md:block"></div>
+
+      <p>
+        <b>{"<Orm/>"}</b>
+        <br />
+        Entity Framework Core
+        <br />
+        Prisma
+        <br />
+        Eloquent
+      </p>
+
+      <p className="mb-0 md:mb-20">
+        <b>{"<Mobile/>"}</b>
+        <br />
+        Flutter
+        <br />
+        Jetpack Compose
+      </p>
+
+      <h3 className="col-span-2 md:col-span-1">
         //TOOLS
       </h3>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        Git<br /> Docker<br /> Vite<br /> Firebase<br /> Swagger/OpenAPI
+
+      <p>
+        Git
+        <br />
+        Docker
+        <br />
+        Vite
+        <br />
+        Firebase
+        <br />
+        Swagger/OpenAPI
       </p>
-      <p className="font-mono text-xl text-justify uppercase tracking-tighter text-gray-400">
-        Redis<br /> Figma<br /> Linux<br /> VS Code<br /> Neovim
+
+      <p>
+        Redis
+        <br />
+        Figma
+        <br />
+        Linux
+        <br />
+        VS Code
+        <br />
+        Neovim
       </p>
     </div>
-  )
+  );
 }
--- src/sections/home.tsx
+++ src/sections/home.tsx
@@ -1,23 +1,63 @@
 export default function Home() {
   return (
-    <div className="flex w-full min-h-screen justify-center items-center gap-4">
-      <div className="flex w-auto h-fit flex-row items-center">
-        <h1 className="font-display text-9xl">
-          Bryan<br />
+    <div className="flex min-h-screen w-full items-center justify-center px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+      <div className="flex w-auto flex-col items-center lg:flex-row">
+
+        <h1
+          className="
+            font-display
+            text-8xl
+            leading-none
+            xl:text-9xl
+            2xl:text-[10rem]
+          "
+        >
+          Bryan
+          <br />
           Antier
         </h1>
-        <div className="flex flex-col font-mono p-5 font-semibold gap-8 h-full ">
+
+        <div
+          className="
+            flex flex-col
+            gap-5
+            p-3
+            font-mono
+            font-semibold
+            sm:gap-6
+            sm:p-4
+            md:gap-7
+            md:p-5
+            lg:gap-8
+            lg:p-6
+            xl:p-8
+          "
+        >
           <div className="flex flex-col">
-            <span className="text-xl">//SOFTWARE ENGINEER</span>
-            <span className="text-xl ml-14">FULL STACK DEVELOPER</span>
+            <span className="text-lg xl:text-xl">
+              //SOFTWARE ENGINEER
+            </span>
+
+            <span className="ml-8 text-lg xl:text-xl">
+              FULL STACK DEVELOPER
+            </span>
           </div>
 
           <div className="flex flex-col">
-            <span className="text-xl">//CONTACT ME @</span>
-            <span className="text-xl ml-14">bryanantier7@gmail.com</span>
-            <span className="text-xl ml-14">(+63) 929 322 4888</span>
+            <span className="text-lg xl:text-xl">
+              //CONTACT ME @
+            </span>
+
+            <span className="ml-8 text-lg xl:text-xl">
+              bryanantier7@gmail.com
+            </span>
+
+            <span className="ml-8 text-lg xl:text-xl">
+              (+63) 929 322 4888
+            </span>
           </div>
         </div>
+
       </div>
     </div>
   )
